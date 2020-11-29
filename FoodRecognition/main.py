73ddcec9a41dd73605cacbf36e8cbfd7762c6653
Developer-Jacob <ldh881113@naver.com>
2020-11-29 17:21:31 +0900

    colab에서 사용할수 있또록 수정

--- FoodRecognition/main.py
+++ FoodRecognition/main.py
@@ -6,35 +6,42 @@ import FoodRecognition.DataSet as DataSet
 from FoodRecognition.Teacher import Student
 from FoodRecognition.Teacher import Teacher
 import torch.nn as nn
+import copy
 
 ROOT_PATH = ''
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def net():
-    # vgg = Models.vgg16(pretrained=True)
-    # vgg.classifier[6] = nn.Linear(4096, 150)
-    mobileNet = Models.mobilenet_v2(pretrained=True)
-    mobileNet.classifier[1] = nn.Linear(1280, 10)
-    return mobileNet
+KEY_OPTIMIZER = "optimizer"
+KEY_CRITERION = "CRITERION"
+KEY_NETWORKS = "networks"
+KEY_EPOCH = "epoch"
+
+class main():
+    def __init__(self, params):
+        self.optimizer = params[KEY_OPTIMIZER]
+        self.criterion = params[KEY_OPTIMIZER]
+        self.epoch = params[KEY_EPOCH]
+        self.newtworks = params[KEY_NETWORKS]
+
+    def imageLoad(self, batch_size):
+        image_data = DataSet.ImageData()
+        train_loader = DataLoader(image_data.trainDataSet(), batch_size=batch_size, num_workers=0)
+        test_loader = DataLoader(image_data.testDataSet(), batch_size=1, num_workers=0)
+
+    def start(self, loader):
+        students = []
+        for network in self.newtworks:
+            students.append(Student(network,
+                                    self.epoch,
+                                    copy.deepcopy(self.optimizer),
+                                    copy.deepcopy(self.criterion), './'))
+        train_loader, test_loader = loader
+        teacher = Teacher(students, train_loader, test_loader)
+        teacher.teach()
 
 if __name__ == "__main__":
     if torch.cuda.is_available():
         print("Using Cuda")
 
-    image_data = DataSet.ImageData()
 
-    train_loader = DataLoader(image_data.trainDataSet(), batch_size=8, num_workers=0)
-    test_loader = DataLoader(image_data.testDataSet(), batch_size=1, num_workers=0)
-    net = net()
-    # for param in net.features.parameters():
-    #     param.requires_grad = False
-    network = net.to(device)
-
-    epoch = 300
-    optimizer = optim.Adam(network.parameters(), lr=0.001)
-    criterion = nn.CrossEntropyLoss().to(device)
-    s1 = Student(network, epoch, optimizer, criterion, './')
-    students = [s1]
-    teacher = Teacher(students, train_loader, test_loader)
-    teacher.teach()
     
