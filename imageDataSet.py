b871ed458ba6bcecf900299629a4cd8947b9758d
Developer-Jacob <ldh881113@naver.com>
2020-12-28 06:35:29 +0900

    Resize 추가

--- imageDataSet.py
+++ imageDataSet.py
@@ -1,13 +1,18 @@
 from torch.utils.data import Dataset
 import torchvision
 
+transform = torchvision.transforms.Compose([
+    torchvision.transforms.Resize((224,224)),
+    torchvision.transforms.ToTensor
+])
+
 class ImageDataSet(Dataset):
     def __init__(self, images, labels):
         self.images = images
         self.labels = labels
 
     def __getitem__(self, index):
-        return torchvision.transforms.ToTensor()(self.images[index]), self.labels[index]
+        return transform(self.images[index]), self.labels[index]
 
     def __len__(self):
         return len(self.images)
